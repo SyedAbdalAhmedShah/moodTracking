cd944457ba9952b0007705d111d915d1e670f8b7
Syed Abdal Shah <82810691+SyedAbdalAhmedShah@users.noreply.github.com>
2024-08-19 21:51:42 +0500

    Initial commit

---
+++ moodTracking/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  moodTracking
+//
+//  Created by Abdal Shah on 19/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ moodTracking/moodTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodTrackingApp.swift
+//  moodTracking
+//
+//  Created by Abdal Shah on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct moodTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
